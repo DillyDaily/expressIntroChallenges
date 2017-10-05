5fe0a344bbff6c214738f678fec228e92d22a53a
DillyDaily <ali.finney@gmail.com>
2017-10-04 19:57:18 -0700

    exercises completed

--- part1/server.js
+++ part1/server.js
@@ -1,10 +1,36 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
+var fs = require('fs');
+var http = require('http');
+var path = require("path");
 
-app.get('/yourroute', function(req, res) {
-  res.send("stuff");
+//1
+app.get('/hello', function(req, res) {
+  res.send("Hello!");
 });
+//2
+app.post('/create/:name/:id', function(req, res) {
+  let obj = {
+    name: req.params.name, 
+    id: req.params.id
+  };
+  res.json(obj);
+})
+//3
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname+"/index.html"))
+});
+
+//4
+app.get('/verify/:age', function(req, res) {
+  let age = parseInt(req.params.age);
+  if (age >=13){
+    res.sendStatus(200);
+  }else{
+    res.sendStatus(403);
+    }
+  });
 
 app.use(function(req, res) {
   res.sendStatus(404);
--- part2/server.js
+++ part2/server.js
@@ -1,10 +1,53 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
+var fs = require('fs');
+var path = require('path');
 
 app.get('/yourroute', function(req, res) {
   res.send("stuff");
 });
+//1
+  app.post('/create/:name/:age', function(req, res) {
+    let obj = {
+      name: req.params.name, 
+      age: parseInt(req.params.age)
+    };
+    fs.readFile('./storage.json', 'utf8', function (err, data) {
+      let usersList = JSON.parse(data);
+      usersList.push(obj);
+
+    fs.writeFile('./storage.json', JSON.stringify(usersList), function(err) {
+      if (err) throw err;
+      res.json(obj);
+      });
+    });
+  });
+
+//2
+  app.get('/', function (req, res) {
+    fs.readFile('./storage.json', 'utf8', function(err, data) {
+      if (err) throw err;
+      let currentData = JSON.parse(data);
+      res.json(currentData);
+    })
+  });
+
+//3
+app.get('/:name', function (req, res) {
+  fs.readFile('./storage.json', 'utf8', function(err, data) {
+    if (err) throw err;
+    let currentData = JSON.parse(data);
+
+   for (let i = 0; i<currentData.length; i++) {
+     if (currentData[i].name === req.params.name) {
+       res.json(currentData[i]);
+       return;
+     }
+   }
+   res.sendStatus(404);
+     });
+  });
 
 app.use(function(req, res) {
   res.sendStatus(404);
